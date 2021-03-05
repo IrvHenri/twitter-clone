509c4118658a82ebb7f22a717083c778b1f846d1
IrvHenri <69181038+IrvHenri@users.noreply.github.com>
2021-03-05 17:23:34 -0500

    installed react router and created pages folder

---
+++ src/pages/mainfeed/Bookmarks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bookmarks = () => {
+  return (
+    <div>
+      <h1>COMING SOON</h1>
+    </div>
+  )
+}
+
+export default Bookmarks
---
+++ src/pages/mainfeed/Explore.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const explore = () => {
+  return (
+    <div>
+      <h1>COMING SOON</h1>
+    </div>
+  );
+};
+
+export default explore;
